f9a9bcbdc5cc013cdb7280146513446956ee60d1
Naveen <naveensreevalsan@gmail.com>
2020-11-01 11:39:48 +0530

    Login route created

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,6 @@
+/* eslint-disable consistent-return */
+/* eslint-disable no-shadow */
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
 const express = require('express');
 
@@ -5,10 +8,7 @@ const router = express.Router();
 const mongoose = require('mongoose');
 
 const User = mongoose.model('User');
-
-router.get('/', (req, res) => {
-  res.send('hello');
-});
+const bcrypt = require('bcryptjs');
 
 // eslint-disable-next-line consistent-return
 router.post('/signup', (req, res) => {
@@ -22,22 +22,49 @@ router.post('/signup', (req, res) => {
       if (savedUser) {
         return res.status(422).json({ error: 'user already exists with that email' });
       }
-      const user = new User({
-        email,
-        password,
-        name,
-      });
+      bcrypt.hash(password, 12)
+        .then((hashedpassword) => {
+          const user = new User({
+            email,
+            password: hashedpassword,
+            name,
+          });
 
-      user.save()
-        .then((user) => {
-          res.json({ message: 'Saved succesfully' });
-        })
-        .catch((err) => {
-          console.log(err);
+          user.save()
+            .then((user) => {
+              res.json({ message: 'Saved succesfully' });
+            })
+            .catch((err) => {
+              console.log(err);
+            });
         });
     })
     .catch((err) => {
       console.log(err);
+    });
+});
+
+router.post('/signin', (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(422).json({ error: 'please add email or password' });
+  }
+  User.findOne({ email })
+    .then((savedUser) => {
+      if (!savedUser) {
+        return res.status(422).json({ error: 'Invalid email or password' });
+      }
+      bcrypt.compare(password, savedUser.password)
+        .then((doMatch) => {
+          if (doMatch) {
+            res.json({ message: 'Succesfuly sined in' });
+          } else {
+            return res.status(422).json({ error: 'Invalid email or password' });
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     });
 });
 
